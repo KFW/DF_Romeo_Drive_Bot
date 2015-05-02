f791a0ce30ec8a58a862d3d38b2d354ff58a5daf
Keith Woeltje <comm@woeltje.org>
2015-05-02 17:34:54 -0500

    clean up variable names

--- dfrdb/dfrdb.ino
+++ dfrdb/dfrdb.ino
@@ -34,77 +34,77 @@ clean up variable names
 Servo wrist;
 Servo claw;
 
-const int int0pin = 3; // interrupt 0 is on pin 3 for wrist (throttle on receiver)
-const int int1pin = 2; // interrupt 1 is on pin 2 for claw (rudder on receiver)
-const int wristpin = 12;
-const int clawpin = 13;
+const int WristInterrupt = 3; // interrupt 0 is on pin 3 for wrist (throttle on receiver)
+const int ClawInterrupt = 2; // interrupt 1 is on pin 2 for claw (rudder on receiver)
+const int WristPin = 12;
+const int ClawPin = 13;
 
-volatile unsigned long wrist_pw0 = 0;   // volatile since will be in interrupt function
-volatile unsigned long claw_pw1 = 0;
-volatile unsigned long start0 = 0;
-volatile unsigned long start1 = 0;
+volatile unsigned long wristPW = 0;   // volatile since will be in interrupt function
+volatile unsigned long clawPW = 0;
+volatile unsigned long wristTimer = 0;
+volatile unsigned long clawTimer = 0;
 volatile boolean flag = false;
-unsigned long wristpulse = 0;
-unsigned long clawpulse = 0;
+unsigned long wristPulse = 0;
+unsigned long clawPulse = 0;
 
-int clawangle = 85;
-int clawminthresh = 1300; // threshold for closing claw; want dead zone in center
-int clawmaxthresh = 1700; // threshold for opening claw
-const int minclawangle = 35; // limit travel to protect servo
-const int maxclawangle = 135;
+int clawAngle = 85;
+int clawMinDeadspaceLimit = 1400; // threshold for closing claw; want dead zone in center
+int clawMaxDeadspaceLimit = 1600; // threshold for opening claw
+const int minClawAngle = 35; // limit travel to protect servo
+const int maxClawAngle = 135;
 
 void setup() {
-  attachInterrupt(0, readint0, CHANGE);  // pin 3 on Romeo  __ signal for wrist
-  attachInterrupt(1, readint1, CHANGE);  // pin 2            __ signal for claw
-  wrist.attach(wristpin);
-  claw.attach(clawpin);
+  attachInterrupt(0, readWristInterrupt, CHANGE);  // pin 3 on Romeo  __ signal for wrist
+  attachInterrupt(1, readClawInterrupt, CHANGE);  // pin 2            __ signal for claw
+  wrist.attach(WristPin);
+  claw.attach(ClawPin);
   wrist.write(90); // starting position
-  wrist.write(clawangle);
+  wrist.write(clawAngle);
 } // end setup()
 
 void loop() {
   // use flag to see if there is actually a new signal; may need to use more specific flags in future
   if (flag){
     noInterrupts(); // pause interrupts so pw signals are read faithfully and not changed part way during assignment
-    wristpulse = wrist_pw0;
-    clawpulse = claw_pw1;
+    wristPulse = wristPW;
+    clawPulse = clawPW;
     flag = false;
     interrupts();   // there is a safer way to do this by saving and resorting settings register, but since I'm not using libraries should be OK
     
-    wrist.writeMicroseconds(wristpulse);
+    wrist.writeMicroseconds(wristPulse);
     
     // for claw want to open/close base on stick, but then hold position
-    if (clawpulse < clawminthresh){
-      clawangle -= 2; // decrement by 2 degrees at a time 
+    if (clawPulse < clawMinDeadspaceLimit){
+      clawAngle -= 1; 
     }
-    else if (clawpulse > clawmaxthresh){
-      clawangle +=2;
+    else if (clawPulse > clawMaxDeadspaceLimit){
+      clawAngle +=1;
     }
-    clawangle = constrain(clawangle, minclawangle, maxclawangle);
-    claw.write(clawangle);
+    clawAngle = constrain(clawAngle, minClawAngle, maxClawAngle);
+    claw.write(clawAngle);
   }
 } // end loop()
 
-void readint0() {
+void readWristInterrupt() {
   // interrupt occurs on CHANGE - need to determine if now high (start of pulse) or low (end of pulse
-  if(digitalRead(int0pin) == HIGH){
-    start0 = micros();  // get time stamp for initiation of pulse
+  if(digitalRead(WristInterrupt) == HIGH){
+    wristTimer = micros();  // get time stamp for initiation of pulse
   }
   else{   
     // pin must now be low - end of pulse; calculate pulse width
-    wrist_pw0 = micros() - start0;
+    wristPW = micros() - wristTimer;
     flag = true;
   }
-} // end readint0()
+} // end readWristInterrupt()
 
-void readint1() {
+void readClawInterrupt() {
   // interrupt occurs on CHANGE - need to determine if now high (start of pulse) or low (end of pulse
-  if(digitalRead(int1pin) == HIGH){
-    start1 = micros();  // get time stamp for initiation of pulse
+  if(digitalRead(ClawInterrupt) == HIGH){
+    clawTimer = micros();  // get time stamp for initiation of pulse
   }
   else{   
     // pin must now be low - end of pulse; calculate pulse width
-    claw_pw1 = micros() - start1;
+    clawPW = micros() - clawTimer;
     flag = true;
   }
-} // end readint1()
+} // end readClawInterrupt()
